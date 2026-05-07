aa4e4df60207acac9a1e99da9f3d874e7aa5d4f4
Ivan Kozlov <ivaan.kozlov@gmail.com>
2026-05-07 00:24:35 -0700

    chore: remove old lua/config and lua/plugins

--- lua/config/remember.lua
+++
@@ -1,5 +0,0 @@
-vim.api.nvim_create_user_command('Remember', function(opts)
-  -- Searches through active mappings for the given command name
-  vim.cmd('filter /' .. opts.args .. '/ map')
-end, { nargs = 1 })
-
--- lua/plugins/init.lua
+++
@@ -1,4 +0,0 @@
-return {
-  "tpope/vim-sleuth",
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-}
