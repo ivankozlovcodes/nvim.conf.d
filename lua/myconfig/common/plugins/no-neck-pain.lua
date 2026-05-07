bfe02082dfa3c437edc9ae1e703df622dc6c58d8
Ivan Kozlov <ivaan.kozlov@gmail.com>
2026-05-07 02:57:56 -0700

    fix: revert to custom autocmd workaround for no-neck-pain/snacks #467

--- lua/myconfig/common/plugins/no-neck-pain.lua
+++ lua/myconfig/common/plugins/no-neck-pain.lua
@@ -1,5 +1,6 @@
 return {
 	"shortcuts/no-neck-pain.nvim",
+	lazy = false,
 	keys = {
 		{ "<leader>cn", "<cmd>NoNeckPain<cr>", desc = "Toggle centered layout" },
 	},
@@ -8,13 +9,25 @@ return {
 	},
 	config = function(_, opts)
 		require("no-neck-pain").setup(opts)
-		vim.api.nvim_create_autocmd("BufReadPost", {
+		-- built-in enableOnVimEnter crashes on Alpha's nil tab (no-neck-pain #467)
+		local group = vim.api.nvim_create_augroup("NoNeckPainAutoEnable", { clear = true })
+
+		local function try_enable()
+			if vim.bo.buftype == "" and vim.fn.expand("%") ~= "" then
+				vim.api.nvim_del_augroup_by_name("NoNeckPainAutoEnable")
+				vim.cmd("NoNeckPain")
+			end
+		end
+
+		vim.api.nvim_create_autocmd("VimEnter", {
+			group = group,
 			once = true,
-			callback = function()
-				if vim.bo.buftype == "" then
-					vim.cmd("NoNeckPain")
-				end
-			end,
+			callback = function() vim.schedule(try_enable) end,
+		})
+
+		vim.api.nvim_create_autocmd("BufEnter", {
+			group = group,
+			callback = try_enable,
 		})
 	end,
 }
