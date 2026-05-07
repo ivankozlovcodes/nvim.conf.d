0f0655a9c1e82042c4cf2675e28c31bb56b81bc5
Ivan Kozlov <ivaan.kozlov@gmail.com>
2026-05-07 02:18:16 -0700

    feat: no-neck-pain keymap + fix left/right asymmetry

--- lua/myconfig/common/plugins/no-neck-pain.lua
+++ lua/myconfig/common/plugins/no-neck-pain.lua
@@ -1,6 +1,15 @@
 return {
   "shortcuts/no-neck-pain.nvim",
+  keys = {
+    { "<leader>cn", "<cmd>NoNeckPain<cr>", desc = "Toggle centered layout" },
+  },
   opts = {
     width = 100,
+    buffers = {
+      right = {
+        -- Compensate for line numbers + signcolumn on left (~6 cols)
+        width = 6,
+      },
+    },
   },
 }
