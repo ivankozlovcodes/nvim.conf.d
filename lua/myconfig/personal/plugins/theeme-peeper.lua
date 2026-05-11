3b141f23fbc82b6eb180294a6cf93128af31ed98
Ivan Kozlov <ivaan.kozlov@gmail.com>
2026-05-10 23:05:38 -0700

    add theme-peeper

---
+++ lua/myconfig/personal/plugins/theeme-peeper.lua
@@ -0,0 +1,41 @@
+return {
+	"JohnnyJumper/theme-peeper.nvim",
+	lazy = false,
+	dependencies = {
+		{
+			"folke/snacks.nvim",
+			opts = {
+				picker = {},
+			},
+		},
+	},
+	keys = {
+		{
+			"<leader>te",
+			function()
+				require("theme_peeper").select()
+			end,
+			desc = "Theme peeper",
+		},
+	},
+	opts = {
+		picker = "snacks",
+		previewer = "float",
+
+		preview = {
+			profile = "code",
+			max_height = 24,
+			border = "rounded",
+			placement = "center",
+		},
+
+		pickers = {
+			snacks = {
+				width = 56,
+				max_height = 12,
+				preview_width = 80,
+				preview_max_height = 24,
+			},
+		},
+	},
+}
