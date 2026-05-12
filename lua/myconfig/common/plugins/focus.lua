9984abb475b31d4f88480e8578a0a4fd23535804
Ivan Kozlov <ivaan.kozlov@gmail.com>
2026-05-11 17:00:40 -0700

    disable focus

--- lua/myconfig/common/plugins/focus.lua
+++ lua/myconfig/common/plugins/focus.lua
@@ -1,19 +1,19 @@
 return {
-  "nvim-focus/focus.nvim",
-  opts = {
-    enable = true,
-    ui = { excluded_filetypes = { "NvimTree", "no-neck-pain" } },
-    autoresize = { enable = true },
-  },
-  config = function(_, opts)
-    require("focus").setup(opts)
-    local group = vim.api.nvim_create_augroup("FocusIgnore", { clear = true })
-    vim.api.nvim_create_autocmd("FileType", {
-      group = group,
-      pattern = { "NvimTree", "no-neck-pain" },
-      callback = function()
-        vim.b.focus_disable = true
-      end,
-    })
-  end,
+	"nvim-focus/focus.nvim",
+	opts = {
+		enable = false,
+		ui = { excluded_filetypes = { "NvimTree", "no-neck-pain" } },
+		autoresize = { enable = true },
+	},
+	config = function(_, opts)
+		require("focus").setup(opts)
+		local group = vim.api.nvim_create_augroup("FocusIgnore", { clear = true })
+		vim.api.nvim_create_autocmd("FileType", {
+			group = group,
+			pattern = { "NvimTree", "no-neck-pain" },
+			callback = function()
+				vim.b.focus_disable = true
+			end,
+		})
+	end,
 }
