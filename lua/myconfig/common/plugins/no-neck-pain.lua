77bf327625245332f1f3e959fb449e9576fce33f
Ivan <ivaan.kozlov@gmail.com>
2026-07-30 17:26:41 -0700

    fix(no-neck-pain): disable auto-enable autocmd by default everywhere
    Agents:
      - author: a4b72982-354e-4451-88cb-c66b76738883
    
    Tested: manual
    
    SCRY=nvim

--- lua/myconfig/common/plugins/no-neck-pain.lua
+++ lua/myconfig/common/plugins/no-neck-pain.lua
@@ -1,6 +1,5 @@
 return {
 	"shortcuts/no-neck-pain.nvim",
-	lazy = false,
 	keys = {
 		{ "<leader>tN", "<cmd>NoNeckPain<cr>", desc = "Toggle centered layout" },
 	},
@@ -11,29 +10,4 @@ return {
 			skipEnteringNoNeckPainBuffer = true,
 		},
 	},
-	config = function(_, opts)
-		require("no-neck-pain").setup(opts)
-		-- built-in enableOnVimEnter crashes on Alpha's nil tab (no-neck-pain #467)
-		local group = vim.api.nvim_create_augroup("NoNeckPainAutoEnable", { clear = true })
-
-		local function try_enable()
-			if vim.bo.buftype == "" and vim.fn.expand("%") ~= "" then
-				vim.api.nvim_del_augroup_by_name("NoNeckPainAutoEnable")
-				vim.cmd("NoNeckPain")
-			end
-		end
-
-		vim.api.nvim_create_autocmd("VimEnter", {
-			group = group,
-			once = true,
-			callback = function()
-				vim.schedule(try_enable)
-			end,
-		})
-
-		vim.api.nvim_create_autocmd("BufEnter", {
-			group = group,
-			callback = try_enable,
-		})
-	end,
 }
